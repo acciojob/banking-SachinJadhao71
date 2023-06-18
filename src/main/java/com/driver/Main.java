aa1e282fe784418ba072fc287bc80adfb8516ee7
sachin jadhao <jadhaosachin7171@gmail.com>
2023-06-18 17:44:32 +0530

    now it

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,6 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount();
+        BankAccount bankAccount = new BankAccount("xyz",5000,0);
     }
 }
